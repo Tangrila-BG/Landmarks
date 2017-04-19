eb2ec85be2f4eebbeeefa9d0d484527918dc555d
Stefani Nikolova <stefani_nik@abv.bg>
2017-04-19 22:44:22 +0300

    Possibly maybe eventually fixed the conflicts

--- DeltaDucks.Web/App_Start/BundleConfig.cs
+++ DeltaDucks.Web/App_Start/BundleConfig.cs
@@ -13,14 +13,12 @@ namespace DeltaDucks.Web
                 "~/Scripts/app/landmarksMap.js",
                 "~/Scripts/app/services/mapService.js",
                 "~/Scripts/app/controllers/mapsController.js",
-                "~/Scripts/app/controlMap.js",
-                "~/Scripts/app/enterCode.js"
+                "~/Scripts/app/controlMap.js"
             ));
 
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/underscore-min.js",
-                        "~/Scripts/sweetalert.min.js",
                         "~/Scripts/moment.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -38,8 +36,7 @@ namespace DeltaDucks.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/animate.css",
-                      "~/Content/sweetalert.css"));
+                      "~/Content/animate.css"));
 
         }
     }
