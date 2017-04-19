2087bebe4dec5e97b4915249e6b559ee4a096629
Stefani Nikolova <stefani_nik@abv.bg>
2017-04-19 12:24:13 +0300

    Improved landmark look and feel

--- DeltaDucks.Web/Scripts/_references.js
+++ DeltaDucks.Web/Scripts/_references.js
@@ -6,6 +6,7 @@
 /// <reference path="jquery-3.1.1.js" />
 /// <reference path="jquery-3.1.1.slim.js" />
 /// <reference path="js/entercode.js" />
+/// <reference path="less-1.5.1.min.js" />
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="respond.js" />
 /// <reference path="respond.matchmedia.addListener.js" />
